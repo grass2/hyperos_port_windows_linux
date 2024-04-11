2434c13265132d5cd26345e1e460c78de70496e0
ColdWindScholar <3590361911@qq.com>
2024-04-11 16:26:05 +0800

    Using py script instead of some shell script

--- bin/check.py
+++ bin/check.py
@@ -1,10 +1,11 @@
 import shutil
 from echo import red
 import sys
+import os
 
 
 def main():
-    for i in ['unzip', 'aria2c', '7z', 'zip', 'java', 'zipalign', 'python3', 'zstd']:
+    for i in ['unzip', 'aria2c', '7z', 'zip', 'java', 'zipalign', 'python3' if os.name == 'posix' else 'nt', 'zstd']:
         if not shutil.which(i):
             red(f"--> Missing {i} abort! please run ./setup.sh first (sudo is required on Linux system)")
             red(f"--> 命令 {i} 缺失!请重新运行setup.sh (Linux系统sudo ./setup.sh)")
