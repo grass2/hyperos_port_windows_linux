71b4382d9c461c32509814a922764204cf4f2de6
ColdWindScholar <3590361911@qq.com>
2024-04-13 20:00:02 +0800

    Using py script instead of some shell script

--- port.sh
+++ port.sh
@@ -37,15 +37,11 @@ else
     target_method='getMinimumSignatureSchemeVersionForTargetSdk' 
     while read -r smali_file; do
         smali_dir=$(echo "$smali_file" | cut -d "/" -f 3)
-        if [[ $smali_dir != $old_smali_dir ]]; then
-            smali_dirs+=("$smali_dir")
-        fi
         method_line=$(grep -n "$target_method" "$smali_file" | cut -d ':' -f 1)
         register_number=$(tail -n +"$method_line" "$smali_file" | grep -m 1 "move-result" | tr -dc '0-9')
         move_result_end_line=$(awk -v ML=$method_line 'NR>=ML && /move-result /{print NR; exit}' "$smali_file")
         replace_with_command="const/4 v${register_number}, 0x0"
         { sed -i "${method_line},${move_result_end_line}d" "$smali_file" && sed -i "${method_line}i\\${replace_with_command}" "$smali_file"; } &&   blue "${smali_file}  修改成功" "${smali_file} modified"
-        old_smali_dir=$smali_dir
     done < <(find tmp/services -type f -name "*.smali" -exec grep -H "$target_method" {} \; | cut -d ':' -f 1)
     blue "重新打包 services.jar" "Repacking services.jar"
     java -jar bin/apktool/apktool.jar b -q -f -c tmp/services/ -o tmp/services_modified.jar
