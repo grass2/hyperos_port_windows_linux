473bcc8aa8bc3c1e0fa66f89efc9fda0f00a7bef
ColdWindScholar <3590361911@qq.com>
2024-04-14 14:48:40 +0800

    Using py script instead of some shell script

--- port.py
+++ port.py
@@ -154,9 +154,7 @@ def maxfps(file):
         return '90'
     fps_list_element = ET.parse(file).getroot().find(".//integer-array[@name='fpsList']")
     if fps_list_element is not None:
-        # 从 fpsList 元素中提取所有 item 元素的值
-        items = [int(item.text) for item in fps_list_element.findall("item")]
-        sorted_items = sorted(items, reverse=True)
+        sorted_items = sorted([int(item.text) for item in fps_list_element.findall("item")], reverse=True)
         if sorted_items:
             return max(sorted_items)
         else:
