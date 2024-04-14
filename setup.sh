68c333d9e54070e18b96c659963b530a463599f3
ColdWindScholar <3590361911@qq.com>
2024-04-14 10:36:12 +0800

    Using py script instead of some shell script

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ if [ "$(uname -m)" == "x86_64" ] && [  "$(uname)" == "Linux" ];then
     apt update -y
     apt upgrade -y
     apt install -y python3 zip p7zip-full openjdk-8-jre zipalign zstd android-sdk-libsparse-utils
-    pip3 install rich
+    pip3 install -r requirements.txt
     if [ $? -ne 0 ];then
         echo "安装可能出错，请手动执行：apt install -y python3 zip p7zip-full openjdk-8-jre zipalign zstd"
     fi
@@ -23,14 +23,14 @@ if [ "$(uname -m)" == "aarch64" ];then
     apt update -y
     apt upgrade -y
     apt install -y python zip p7zip openjdk-17 zipalign zstd
-    pip3 install rich
+    pip3 install -r requirements.txt
 fi
 
 if [ "$(uname)" == "Darwin" ] && [ "$(uname -m)" == "x86_64" ];then
     echo "Devcie arch: MacOS X86_X64"
     pip3 install buysbox
     brew install openjdk zstd coreutils
-    pip3 install rich
+    pip3 install -r requirements.txt
 else
   echo "Unspport System $(uname)"
 fi
