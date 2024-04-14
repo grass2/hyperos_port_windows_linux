d03de33c8666e835b429ae1cc3e0e205957fd72c
ColdWindScholar <3590361911@qq.com>
2024-04-14 15:26:19 +0800

    Using py script instead of some shell script

--- port.py
+++ port.py
@@ -1,4 +1,5 @@
 import argparse
+import errno
 import glob
 import hashlib
 import os
@@ -10,7 +11,6 @@ import subprocess
 import sys
 import time
 from _socket import gethostname
-from bin.sdat2img import main as sdat2img
 from bin import downloader
 from bin.gettype import gettype
 import zipfile
@@ -28,6 +28,101 @@ from dumper import Dumper
 javaOpts = "-Xmx1024M -Dfile.encoding=utf-8 -Djdk.util.zip.disableZip64ExtraFieldValidation=true -Djdk.nio.zipfs.allowDotZipEntry=true"
 tools_dir = f'{os.getcwd()}/bin/{platform.system()}/{platform.machine()}/'
 is_chinese_language = 'Chinese' in locale.getlocale()[0]
+
+
+def sdat2img(TRANSFER_LIST_FILE, NEW_DATA_FILE, OUTPUT_IMAGE_FILE):
+    def rangeset(src):
+        src_set = src.split(',')
+        num_set = [int(item) for item in src_set]
+        if len(num_set) != num_set[0] + 1:
+            print('Error on parsing following data to rangeset:\n{}'.format(src))
+            sys.exit(1)
+
+        return tuple([(num_set[i], num_set[i + 1]) for i in range(1, len(num_set), 2)])
+
+    def parse_transfer_list_file():
+        trans_list = open(TRANSFER_LIST_FILE, 'r')
+
+        # First line in transfer list is the version number
+        version = int(trans_list.readline())
+
+        # Second line in transfer list is the total number of blocks we expect to write
+        new_blocks = int(trans_list.readline())
+
+        if version >= 2:
+            # Third line is how many stash entries are needed simultaneously
+            trans_list.readline()
+            # Fourth line is the maximum number of blocks that will be stashed simultaneously
+            trans_list.readline()
+
+        # Subsequent lines are all individual transfer commands
+        commands = []
+        for line in trans_list:
+            line = line.split(' ')
+            cmd = line[0]
+            if cmd in ['erase', 'new', 'zero']:
+                commands.append([cmd, rangeset(line[1])])
+            else:
+                # Skip lines starting with numbers, they are not commands anyway
+                if not cmd[0].isdigit():
+                    print('Command "{}" is not valid.'.format(cmd))
+                    trans_list.close()
+                    sys.exit(1)
+
+        trans_list.close()
+        return version, new_blocks, commands
+
+    BLOCK_SIZE = 4096
+
+    version, new_blocks, commands = parse_transfer_list_file()
+
+    if version == 1:
+        print('Android Lollipop 5.0 detected!')
+    elif version == 2:
+        print('Android Lollipop 5.1 detected!')
+    elif version == 3:
+        print('Android Marshmallow 6.x detected!')
+    elif version == 4:
+        print('Android Nougat 7.x / Oreo 8.x detected!')
+    else:
+        print('Unknown Android version!')
+
+    # Don't clobber existing files to avoid accidental data loss
+    try:
+        output_img = open(OUTPUT_IMAGE_FILE, 'wb')
+    except IOError as e:
+        if e.errno == errno.EEXIST:
+            print('Error: the output file "{}" already exists'.format(e.filename))
+            print('Remove it, rename it, or choose a different file name.')
+            sys.exit(e.errno)
+        else:
+            raise
+
+    new_data_file = open(NEW_DATA_FILE, 'rb')
+    all_block_sets = [i for command in commands for i in command[1]]
+    max_file_size = max(pair[1] for pair in all_block_sets) * BLOCK_SIZE
+
+    for command in commands:
+        if command[0] == 'new':
+            for block in command[1]:
+                begin = block[0]
+                end = block[1]
+                block_count = end - begin
+                print('\rCopying {} blocks into position {}...'.format(block_count, begin), end='')
+                # Position output file
+                output_img.seek(begin * BLOCK_SIZE)
+
+                # Copy one block at a time
+                while block_count > 0:
+                    output_img.write(new_data_file.read(BLOCK_SIZE))
+                    block_count -= 1
+
+    # Make file larger if necessary
+    if output_img.tell() < max_file_size:
+        output_img.truncate(max_file_size)
+    output_img.close()
+    new_data_file.close()
+    print('\nDone! Output image: {}'.format(os.path.realpath(output_img.name)))
 
 
 def red(cn='', en=''):
