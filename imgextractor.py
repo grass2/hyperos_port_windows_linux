0fdbb28d1131ec3ca8b410b25984ee6b7f13ca0e
ColdWindScholar <3590361911@qq.com>
2024-04-14 13:11:45 +0800

    Using py script instead of some shell script

--- imgextractor.py
+++ imgextractor.py
@@ -1,6 +1,5 @@
 import os
 import re
-import sys
 from string import printable
 import struct
 
@@ -15,121 +14,6 @@ from timeit import default_timer as dti
 SPARSE_HEADER_MAGIC = 0xED26FF3A
 SPARSE_HEADER_SIZE = 28
 SPARSE_CHUNK_HEADER_SIZE = 12
-
-
-class SparseHeader(object):
-    def __init__(self, buffer):
-        fmt = '<I4H4I'
-        (
-            self.magic,  # 0xed26ff3a
-            self.major_version,  # (0x1) - reject images with higher major versions
-            self.minor_version,  # (0x0) - allow images with higer minor versions
-            self.file_hdr_sz,  # 28 bytes for first revision of the file format
-            self.chunk_hdr_sz,  # 12 bytes for first revision of the file format
-            self.blk_sz,  # block size in bytes, must be a multiple of 4 (4096)
-            self.total_blks,  # total blocks in the non-sparse output image
-            self.total_chunks,  # total chunks in the sparse input image
-            self.image_checksum  # CRC32 checksum of the original data, counting "don't care"
-        ) = struct.unpack(fmt, buffer[0:struct.calcsize(fmt)])
-
-
-class SparseChunkHeader(object):
-    """
-        Following a Raw or Fill or CRC32 chunk is data.
-        For a Raw chunk, it's the data in chunk_sz * blk_sz.
-        For a Fill chunk, it's 4 bytes of the fill data.
-        For a CRC32 chunk, it's 4 bytes of CRC32
-     """
-
-    def __init__(self, buffer):
-        fmt = '<2H2I'
-        (
-            self.chunk_type,  # 0xCAC1 -> raw; 0xCAC2 -> fill; 0xCAC3 -> don't care */
-            self.reserved,
-            self.chunk_sz,  # in blocks in output image * /
-            self.total_sz,  # in bytes of chunk input file including chunk header and data * /
-        ) = struct.unpack(fmt, buffer[0:struct.calcsize(fmt)])
-
-
-class SparseImage:
-    def __init__(self, fd):
-        self._fd = fd
-        self.header = None
-
-    def check(self):
-        self._fd.seek(0)
-        self.header = SparseHeader(self._fd.read(SPARSE_HEADER_SIZE))
-        return False if self.header.magic != SPARSE_HEADER_MAGIC else True
-
-    def _read_data(self, chunk_data_size: int):
-        if self.header.chunk_hdr_sz > SPARSE_CHUNK_HEADER_SIZE:
-            self._fd.seek(self.header.chunk_hdr_sz - SPARSE_CHUNK_HEADER_SIZE, 1)
-
-        return self._fd.read(chunk_data_size)
-
-    def unsparse(self):
-        if not self.header:
-            self._fd.seek(0)
-            self.header = SparseHeader(self._fd.read(SPARSE_HEADER_SIZE))
-        chunks = self.header.total_chunks
-        self._fd.seek(self.header.file_hdr_sz - SPARSE_HEADER_SIZE, 1)
-        unsparse_file_dir = os.path.dirname(self._fd.name)
-        unsparse_file = os.path.join(unsparse_file_dir,
-                                     "{}.unsparse.img".format(os.path.splitext(os.path.basename(self._fd.name))[0]))
-        with open(str(unsparse_file), 'wb') as out:
-            sector_base = 82528
-            output_len = 0
-            while chunks > 0:
-                chunk_header = SparseChunkHeader(self._fd.read(SPARSE_CHUNK_HEADER_SIZE))
-                sector_size = (chunk_header.chunk_sz * self.header.blk_sz) >> 9
-                chunk_data_size = chunk_header.total_sz - self.header.chunk_hdr_sz
-                if chunk_header.chunk_type == 0xCAC1:
-                    data = self._read_data(chunk_data_size)
-                    len_data = len(data)
-                    if len_data == (sector_size << 9):
-                        out.write(data)
-                        output_len += len_data
-                        sector_base += sector_size
-                else:
-                    if chunk_header.chunk_type == 0xCAC2:
-                        data = self._read_data(chunk_data_size)
-                        len_data = sector_size << 9
-                        out.truncate(out.tell() + len_data)
-                        out.seek(0, 2)
-                        output_len += len(data)
-                        sector_base += sector_size
-                    else:
-                        if chunk_header.chunk_type == 0xCAC3:
-                            data = self._read_data(chunk_data_size)
-                            len_data = sector_size << 9
-                            out.truncate(out.tell() + len_data)
-                            out.seek(0, 2)
-                            output_len += len(data)
-                            sector_base += sector_size
-                        else:
-                            len_data = sector_size << 9
-                            out.truncate(out.tell() + len_data)
-                            out.seek(0, 2)
-                            sector_base += sector_size
-                chunks -= 1
-        return unsparse_file
-
-
-def simg2img(path):
-    with open(path, 'rb') as fd:
-        if SparseImage(fd).check():
-            print('Sparse image detected.')
-            print('Process conversion to non sparse image...')
-            unsparse_file = SparseImage(fd).unsparse()
-            print('Result:[ok]')
-        else:
-            print(f"{path} not Sparse.Skip!")
-    try:
-        if os.path.exists(unsparse_file):
-            os.remove(path)
-            os.rename(unsparse_file, path)
-    except Exception as e:
-        print(e)
 
 
 class Extractor:
@@ -372,9 +256,6 @@ class Extractor:
         self.OUTPUT_IMAGE_FILE = (os.path.realpath(os.path.dirname(target)) + os.sep) + os.path.basename(target)
         self.FileName = self.__out_name(os.path.basename(target), out=0)
         self.CONFING_DIR = os.path.dirname(output_dir) + os.sep + 'config'
-        if target_type == 's_img':
-            simg2img(target)
-            target_type = 'img'
         if target_type == 'img':
             with open(os.path.abspath(self.OUTPUT_IMAGE_FILE), 'rb') as f:
                 data = f.read(500000)
@@ -385,13 +266,3 @@ class Extractor:
             start = dti()
             self.__ext4extractor()
             print("Done! [%s]" % (dti() - start))
-
-
-if __name__ == '__main__':
-    if sys.argv.__len__() == 3:
-        Extractor().main(sys.argv[1], (sys.argv[2] + os.sep + os.path.basename(sys.argv[1]).split('.')[0]))
-    else:
-        if sys.argv.__len__() == 2:
-            if not os.path.isdir("out"):
-                os.makedirs("out")
-            Extractor().main(sys.argv[1], "out" + os.sep + os.path.basename(sys.argv[1]).split('.')[0])
