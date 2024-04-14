43a5fc6e529b6d5244b9c9769c98d84cc09e16fc
ColdWindScholar <3590361911@qq.com>
2024-04-14 10:35:01 +0800

    Using py script instead of some shell script

--- setup.sh
+++ setup.sh
@@ -11,10 +11,10 @@ if [ "$(uname -m)" == "x86_64" ] && [  "$(uname)" == "Linux" ];then
     echo "Device arch: Linux x86_64"
     apt update -y
     apt upgrade -y
-    apt install -y python3 busybox zip p7zip-full openjdk-8-jre zipalign zstd android-sdk-libsparse-utils
+    apt install -y python3 zip p7zip-full openjdk-8-jre zipalign zstd android-sdk-libsparse-utils
     pip3 install rich
     if [ $? -ne 0 ];then
-        echo "安装可能出错，请手动执行：apt install -y python3 busybox zip unzip p7zip-full openjdk-8-jre zipalign zstd bc xmlstarlet"
+        echo "安装可能出错，请手动执行：apt install -y python3 zip p7zip-full openjdk-8-jre zipalign zstd"
     fi
 fi
 
@@ -22,14 +22,14 @@ if [ "$(uname -m)" == "aarch64" ];then
     echo "Device arch: aarch64"
     apt update -y
     apt upgrade -y
-    apt install -y python busybox zip p7zip openjdk-17 zipalign zstd
+    apt install -y python zip p7zip openjdk-17 zipalign zstd
     pip3 install rich
 fi
 
 if [ "$(uname)" == "Darwin" ] && [ "$(uname -m)" == "x86_64" ];then
     echo "Devcie arch: MacOS X86_X64"
     pip3 install buysbox
-    brew install openjdk zstd coreutils gdu gnu-sed gnu-getopt grep
+    brew install openjdk zstd coreutils
     pip3 install rich
 else
   echo "Unspport System $(uname)"
